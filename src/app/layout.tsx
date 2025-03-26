c5b9a0bad20ebacce47d1276e107301a1762b34c
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2025-03-26 12:12:12 +0000

    add font + small fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Atkinson_Hyperlegible } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const atkinsonHyperlegible = Atkinson_Hyperlegible({
+  weight: ['400', '700'],
   subsets: ['latin'],
 });
 
@@ -24,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${atkinsonHyperlegible.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
